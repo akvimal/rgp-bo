ac2ef30b1279b00d94a3d40f38f37507716d681c
Vimal Krishnan <vimalkrishnan@Vimals-MacBook-Air.local>
2024-12-12 23:05:43 +0530

    updated

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  // apiHost: 'http://89.233.104.2:30001'
-  apiHost: 'http://127.0.0.1:30001'
+  apiHost: 'http://89.233.104.2:30001'
+  // apiHost: 'http://127.0.0.1:30001'
 };
